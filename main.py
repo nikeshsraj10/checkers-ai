91de71f1cc3de8be25e9eebb64c73d37ba9c3b94
Nikesh S Raj <nikeshsraj@gmail.com>
2020-12-13 17:57:51 -0500

    1. Deleted old files 2. Added data files

--- main.py
+++
@@ -1,9 +0,0 @@
-"""
-    This module is used to start the game and control the flow of the program
-"""
-import game
-
-if __name__ == '__main__':
-    print("----------------------Play American Checkers-----------------------")
-    game = game.Game()
-    game.start_game()
